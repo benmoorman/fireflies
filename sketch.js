5772cc61cd228b3d85b99138da5c9fe783cdc1a0
Ben Moorman <bemn67@gmail.com>
2023-05-29 09:28:09 -0400

    Initial firefly logic

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+let fireflies = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(720, 400);
+
+  for (let i = 0; i < 250; i += 1) {
+    fireflies.push(new Firefly());
+  }
+
+  for (let i = 0; i < 1000; i++) {
+    draw();
+  }
 }
 
 function draw() {
-  background(220);
+  background('#0f0f0f');
+  for (let i = 0; i < fireflies.length; i++) {
+    fireflies[i].createFirefly();
+    // fireflies[i].moveFirefly();
+  }
 }
